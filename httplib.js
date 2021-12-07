4b0b52a67171fccf24f8edd3764752c3c70660d0
Guru-1307 <guruvisnu92@gmail.com>
2021-12-07 16:21:17 +0530

    initial

---
+++ httplib.js
@@ -0,0 +1,26 @@
+const http=require('http');
+const fs=require('fs');
+
+http.createServer((req,res) =>{
+    res.writeHead(200,{'Content-Type':'text/html'});
+
+    if(req.url ==='/home' || req.url ==='/' ){
+        let data =fs.readFileSync('Static/index.html');
+        res.write(data);
+
+    
+    }else if(req.url ==='/login' ){
+        let data =fs.readFileSync('Static/login.html');
+        res.write(data);
+    }else if(req.url ==='/product' ){
+        let data =fs.readFileSync('Static/product.html');
+        res.write(data);
+    
+
+    }else{
+        let data =fs.readFileSync('Static/error.html');
+        res.write(data);
+    }
+    res.end();
+}).listen(process.env.PORT,()=>{console.log('server started at port 8080')});
+
